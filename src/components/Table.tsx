ca31c188808f979a4853027b67025e567549397d
Adam-Kosciukiewicz <adamkosciukiewiczkontakt@gmail.com>
2024-09-14 13:59:08 +0200

    add searchbar

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,13 +3,15 @@ import { QueryKeys, QueryStatus } from "../query/consts";
 import { fetchUsers } from "../query/fetchData";
 import { errorHandler } from "../query/errorHandler";
 import UserRow from "../elements/UserRow";
+import SearchBar from "../elements/SearchBar";
 
 const Table: React.FC = (): JSX.Element => {
     const { data, status } = useQuery(QueryKeys.USERS, fetchUsers);
 
 
     return errorHandler(status as QueryStatus, (
-        <div>
+        <div>  
+            <SearchBar></SearchBar>
             <table>
                 <tr>
                     <th>Name</th>
